d177692f573c6bcbb5ec4c26a9a2fae476a0c4f6
Sam Goddard <hello@samgoddard.co.uk>
2021-06-26 19:50:02 +0100

    testing build times

--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,9 @@ export default function Home() {
         >
           <Container>
             <m.div variants={fade}>
-              <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl mb-4">Home Page</h1>
+              <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl mb-4">Jason O'Rear</h1>
               <div className="content max-w-3xl mb-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
                 <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
