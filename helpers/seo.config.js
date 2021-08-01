69f094047199631b0fbf6cb75b04544e35a7ea9e
Sam Goddard <hello@samgoddard.co.uk>
2021-08-01 19:40:05 +0100

    seo, favicon and social share pass

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,12 +1,20 @@
 export default {
-  defaultTitle: `Jason O'Rear | Architectural Photography`,
+  defaultTitle: `Jason O'Rear | Architectural Photographer`,
   description: 'Jason O’Rear is a professional photographer whose work celebrates the creativity and ingenuity of contemporary architectural design.',
   titleTemplate: `%s | Jason O'Rear`,
   openGraph: {
     type: 'website',
     locale: 'en_GB',
     url: 'https://jason-orear.vercel.app/', // -> https://jasonorear.com/
-    site_name: `Jason O'Rear - Architectural Photography`,
+    site_name: `Jason O'Rear - Architectural Photographer`,
+    images: [
+      {
+        url: '/images/social.jpg',
+        width: 1200,
+        height: 630,
+        alt: `Jason O'Rear Portrait`,
+      },
+    ],
   },
   twitter: {
     site: '@site',
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head/>
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
