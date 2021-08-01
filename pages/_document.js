7cec175b2bebb2b0aee87df585b57d0fc44ded5c
Sam Goddard <hello@samgoddard.co.uk>
2021-08-01 19:42:23 +0100

    seo, favicon and social share pass

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
